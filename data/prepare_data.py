56163c88ee0b95ff0e2ecdba6debd45adce8d02e
Jarek Potiuk <jarek@potiuk.com>
2014-06-22 10:13:23 +0200

    Formatting improved

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -6,6 +6,7 @@ import random
 
 from configuration import rooms, room_names, MARGIN_HEIGHT, MARGIN_WIDTH
 
+
 def get_list_element(source, index):
     if len(source) <= index:
         source.append({})
@@ -28,6 +29,7 @@ def process_room(source, row, room):
             else:
                 element[room] = int(value)
 
+
 def read_file(source):
     with open('MCE-wifi-activity.csv', 'rt') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -35,15 +37,16 @@ def read_file(source):
             if row[0] == 'HOST':
                 process_times(source, row)
             elif row[0] == 'H':
-                process_room(source, row,"hall")
+                process_room(source, row, "hall")
             elif row[0] == 'S1':
-                process_room(source, row,"room-1")
+                process_room(source, row, "room-1")
             elif row[0] == 'S2':
-                process_room(source, row,"room-2")
+                process_room(source, row, "room-2")
             elif row[0] == 'S3':
-                process_room(source, row,"room-3")
+                process_room(source, row, "room-3")
             elif row[0] == 'Total':
-                process_room(source, row,"total")
+                process_room(source, row, "total")
+
 
 def add_with_interpolations(last_element, new_element, list):
     extended_room_names = room_names[:]
@@ -55,12 +58,13 @@ def add_with_interpolations(last_element, new_element, list):
                 full_difference = new_element[room_name] - last_element[room_name]
                 interpolated_difference = full_difference * i / 5
                 interpolated_element[room_name] += interpolated_difference
-            last_time_digit = last_element['time'][4]
+        last_time_digit = last_element['time'][4]
         interpolated_element['time'] = last_element['time'][:4] + \
                                        chr(ord(last_time_digit) + i) + \
                                        last_element['time'][5:]
         list.append(interpolated_element)
     list.append(new_element)
+
 
 def perform_interpolation(source):
     last_element = source[0]
@@ -71,8 +75,10 @@ def perform_interpolation(source):
         last_element = new_element
     return new_source
 
+
 data_source = []
 snapshots = []
+
 
 def fill_room_with_people(snapshot, room_name, no_of_people, previous_snapshot):
     if previous_snapshot is not None:
@@ -81,7 +87,7 @@ def fill_room_with_people(snapshot, room_name, no_of_people, previous_snapshot):
         people_list = []
     room = rooms[room_name]
     if len(people_list) < no_of_people:
-        for i in range(len(people_list),no_of_people):
+        for i in range(len(people_list), no_of_people):
             random_x_offset = random.randint(0, room['width'] - 2 * MARGIN_WIDTH - 1)
             random_y_offset = random.randint(0, room['height'] - 2 * MARGIN_HEIGHT - 1)
             person = {
@@ -94,17 +100,20 @@ def fill_room_with_people(snapshot, room_name, no_of_people, previous_snapshot):
         people_list = people_list[:no_of_people]
     snapshot[room_name] = people_list
 
+
 last_snapshot = None
+
 
 def calculate_exits(room_from_name, room_to_name, exit_name, source_old, source_new):
     source_adjusted = source_new.copy()
     difference = source_old[room_from_name] - source_new[room_from_name]
     from_adjustment = difference / 2
     to_adjustment = difference - from_adjustment
-    source_adjusted[room_from_name] = max(0,source_new[room_from_name] - abs(from_adjustment))
-    source_adjusted[room_to_name] = max(0,source_new[room_to_name] - abs(to_adjustment))
+    source_adjusted[room_from_name] = max(0, source_new[room_from_name] - abs(from_adjustment))
+    source_adjusted[room_to_name] = max(0, source_new[room_to_name] - abs(to_adjustment))
     source_adjusted[exit_name] = abs(difference)
     return source_adjusted
+
 
 def adjust_total_exit(source_old, source_new):
     source_adjusted = source_new.copy()
@@ -112,6 +121,7 @@ def adjust_total_exit(source_old, source_new):
     source_adjusted['exit'] = abs(difference)
     source_adjusted['hall'] = max(0, source_new['hall'] - abs(difference))
     return source_adjusted
+
 
 def adjust_data_source_with_exits(source):
     source_old = None
@@ -131,6 +141,7 @@ def adjust_data_source_with_exits(source):
             source_new['room-2-exit'] = 0
             source_new['room-3-exit'] = 0
 
+
 def fill_data(source, measurement_index):
     global last_snapshot
     snapshot = {}
@@ -147,8 +158,10 @@ def fill_data(source, measurement_index):
     last_snapshot = snapshot
     snapshots.append(snapshot)
 
+
 read_file(data_source)
 data_source = perform_interpolation(data_source)
+
 
 def write_json_file(name, data):
     with open(name, 'wt') as outfile:

809ce14200731c745693d4c1c60f5f78f3b871b4
Jarek Potiuk <jarek@potiuk.com>
2014-08-13 23:59:45 +0200

    Black background

--- data/prepare_image.py
+++ data/prepare_image.py
@@ -1,21 +1,25 @@
 from configuration import rooms, IMAGE_WIDTH, IMAGE_HEIGHT, FILL_COLOUR, room_names, MARGIN_WIDTH, MARGIN_HEIGHT, \
-    MARGIN_COLOUR
+    MARGIN_COLOUR, OUTLINE_COLOUR, BACKGROUND_COLOUR
 import os
 
 
 def draw_room(draw, room_name):
     room = rooms[room_name]
-    draw.rectangle([(room['x'], room['y']),
-                    (room['x']+room['width']+1, room['y']+room['height']+1)],
-                   fill=FILL_COLOUR, outline='black')
-    draw.rectangle([(room['x'] + MARGIN_WIDTH, room['y'] + MARGIN_HEIGHT),
-                    (room['x']+room['width']+1 - MARGIN_WIDTH, room['y']+room['height']+1 - MARGIN_HEIGHT)],
-                   fill=None, outline=MARGIN_COLOUR)
+    left = room['x']
+    top = room['y']
+    right = room['x']+room['width']+1
+    bottom = room['y']+room['height']+1
+    draw.rectangle([(left, top), (right, bottom)], fill=FILL_COLOUR)
+    draw.line([(left, top), (right, top), (right, bottom), (left, bottom), (left, top)], fill=OUTLINE_COLOUR, width=2)
+
+    # draw.rectangle([(room['x'] + MARGIN_WIDTH, room['y'] + MARGIN_HEIGHT),
+    #               (room['x']+room['width']+1 - MARGIN_WIDTH, room['y']+room['height']+1 - MARGIN_HEIGHT)],
+    #               fill=None, outline=MARGIN_COLOUR)
 
 def generate_image():
     from PIL import Image, ImageDraw
     size = (IMAGE_WIDTH, IMAGE_HEIGHT)
-    im = Image.new('RGB', size, "white")
+    im = Image.new('RGB', size, BACKGROUND_COLOUR)
     draw = ImageDraw.Draw(im)
     for room_name in room_names[::-1]: # revert array to show better exits
         draw_room(draw, room_name)
